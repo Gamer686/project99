d846714e1f91e2fe6ab683c2b689ee07d6e9f94a
Gamer686 <74534832+Gamer686@users.noreply.github.com>
2022-07-12 13:04:11 +0530

    Add files via upload

--- BackUpfiles.py
+++ BackUpfiles.py
@@ -1,35 +1,42 @@
 import os
 import shutil
 
-# Write the name of the directory here,
-# that needs to get sorted
-# path = '/home/rajeev/Videos'
-path = input("enter the name of the directory to be sorted :- ")
+def main():
+    deleted_folders_count = 0
+    deleted_files_count = 0
+    path = "Path_To_Delete"
+    days = 30
+    seconds = time.time() - (days * 24 * 60 * 60)
 
-# This will create a properly organized
-# list with all the filename that is
-# there in the directory
-list_of_files = os.listdir(path)
+    if os.path.exists(path):
+     for root_folder, folders , files in os.walk(path):
 
-# This will go through each and every file
-for file in list_of_files:
-    name, ext = os.path.splitext(file)
+        if seconds >= get_file_or_folder_age(root_folder):
+            remove_folder(root_folder)
+        deleted_folders_count += 1
 
-    # This is going to store the extension type
-    ext = ext[1:]
+        break
 
-    # This forces the next iteration,
-    # if it is the directory
-    if ext == '':
-        continue
+    else: 
+        for folder in folders:
+                folder_path = os.path.join(root_folder,folder)
+                if seconds >= get_file_or_folder_age(file_path):
+                    remove_file(file_path)
+                deleted_files_count  += 1
 
-    # This will move the file to the directory
-    # where the name 'ext' already exists
-    if os.path.exists(path+'/'+ext):
-       shutil.move(path+'/'+file, path+'/'+ext+'/'+file)
+        print(f"Total folders deleted: {deleted_folders_count}") 
+        print(f"Total files deleted: {deleted_files_count}")
 
-    # This will create a new directory,
-    # if the directory does not already exist
-    else:
-        os.makedirs(path+'/'+ext)
-        shutil.move(path+'/'+file, path+'/'+ext+'/'+file)
+    def remove_folder(path): 
+        if  not os.remove(path):
+            print(f"(path) is removed succesfully") 
+
+        else:
+            print("Unable to delete the "+path)    
+
+    def get_file_or_folder_age(path):
+      ctime = os.stat(path).at_ctm
+      return ctime
+
+if __name__ == "__main__" :
+    main() 
